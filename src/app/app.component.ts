ecc89906a9ea1170d7a3e372c7c9b37048e3562d
Ricardo Tovar <ricardotovar.grupobruild@gmail.com>
2021-01-02 18:39:23 -0500

    add ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'frontend';
   sidenavAdmin: any[];
+  valor = true;
 
   constructor() {
     this.sidenavAdmin = [
       {
         label: 'Inicio',
         routerLink: '/',
+        clase: 'nav-icon fas fa-th',
       },
       {
         label: 'Resultados',
         routerLink: '/result',
+        clase: 'nav-icon fas fa-table',
       },
     ];
+  }
+
+  returnClass(clase) {
+    return clase;
   }
 }
